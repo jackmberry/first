8be5c63e1e790ee872f022c516f077da88e68cc2
jack <jackmberry@googlemail.com>
2019-08-13 13:24:01 +0100

    adding first game data

---
+++ FirstTests/FirstTests.swift
@@ -0,0 +1,34 @@
+//
+//  FirstTests.swift
+//  FirstTests
+//
+//  Created by Jack Berry on 05/08/2019.
+//  Copyright © 2019 Jack M Berry. All rights reserved.
+//
+
+import XCTest
+@testable import First
+
+class FirstTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
